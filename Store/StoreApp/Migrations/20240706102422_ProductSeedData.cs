9c0b5d75f1ab4feff948355563ff82e2327cd185
Bunyamin Yavuz <1b.yavuz07@gmail.com>
2024-07-06 13:46:37 +0300

    Created database with migrations, added seed data with migrations, and updated the database

---
+++ Store/StoreApp/Migrations/20240706102422_ProductSeedData.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreApp.Migrations
+{
+    public partial class ProductSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Price", "ProductName" },
+                values: new object[] { 1, 30000m, "Computer" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Price", "ProductName" },
+                values: new object[] { 2, 3000m, "Keyboard" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Price", "ProductName" },
+                values: new object[] { 3, 800m, "Mouse" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Price", "ProductName" },
+                values: new object[] { 4, 20000m, "Monitor" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Price", "ProductName" },
+                values: new object[] { 5, 5000m, "Deck" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 5);
+        }
+    }
+}
--- Store/StoreApp/Models/RepositoryContext.cs
+++ Store/StoreApp/Models/RepositoryContext.cs
@@ -10,5 +10,18 @@ namespace StoreApp.Models
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+            .HasData(
+                new Product() { ProductId = 1, ProductName = "Computer", Price = 30_000 },
+                new Product() { ProductId = 2, ProductName = "Keyboard", Price = 3_000 },
+                new Product() { ProductId = 3, ProductName = "Mouse", Price = 800 },
+                new Product() { ProductId = 4, ProductName = "Monitor", Price = 20_000 },
+                new Product() { ProductId = 5, ProductName = "Deck", Price = 5_000 }
+            );
+        }
     }
 }
