7354c7ffed29bd96401bf95c5fd2258ab09b73c4
Bunyamin Yavuz <1b.yavuz07@gmail.com>
2024-07-06 14:50:42 +0300

    Implemented dependency injection pattern to fetch data from database in Controller and pass it to the View

---
+++ Store/StoreApp/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using StoreApp.Models;
+
+namespace StoreApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly RepositoryContext _context;
+
+        public ProductController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _context.Products?.ToList();
+            return View(model);
+        }
+    }
+}
