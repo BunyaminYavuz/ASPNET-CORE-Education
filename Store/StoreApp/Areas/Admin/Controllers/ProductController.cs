d00a5ca5fec35a22034cb10f60487f49634814a3
Bunyamin Yavuz <1b.yavuz07@gmail.com>
2024-07-16 19:34:14 +0300

    Added product retrieval and table display in Products action linked from admin area navbar.

--- Store/StoreApp/Areas/Admin/Controllers/ProductController.cs
+++ Store/StoreApp/Areas/Admin/Controllers/ProductController.cs
@@ -1,13 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
 
 namespace StoreApp.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
     {
+        private readonly IServiceManager _manager;
+
+        public ProductController(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var products = _manager.ProductService.GetAllProducts(false);
+            return View(products);
         }
     }
 }
